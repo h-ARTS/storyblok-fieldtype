7a4093a051854a52d7888e4b42424e04f94506d9
Hanan <hi5hanan@yahoo.com>
2021-12-09 14:04:22 +0100

    changes

--- src/Plugin.vue
+++ src/Plugin.vue
@@ -1,20 +1,6 @@
 <template>
   <div>
-    Google snippet preview:
-    <div class="p-metatags__preview">
-      <div class="p-metatags__google-title">{{ model.title || 'Your title' }}</div>
-      <div class="p-metatags__google-link">yoursite.com/example</div>
-      <div class="p-metatags__google-description">{{ model.description || 'Your description' }}</div>
-    </div>
-    <div class="uk-form-row">
-      <label>Meta Title</label>
-      <input type="text" placeholder="Your title" v-model="model.title" class="uk-width-1-1">
-    </div>
-
-    <div class="uk-form-row">
-      <label>Meta description</label>
-      <textarea rows="4" placeholder="Your description" v-model="model.description" class="uk-width-1-1"></textarea>
-    </div>
+    <textarea v-model="model.content" id="mytextarea"></textarea>
   </div>
 </template>
 
@@ -24,42 +10,232 @@ export default {
   methods: {
     initWith() {
       return {
-        // needs to be equal to your storyblok plugin name
-        plugin: 'my-plugin-name',
-        title: '',
-        description: ''
-      }
+        plugin: "wysiwyg-tinymce5",
+        content: ""
+      };
     },
     pluginCreated() {
-      // eslint-disable-next-line
-      console.log('View source and customize: https://github.com/storyblok/storyblok-fieldtype')
+      this.$sb.getScript(
+        "https://cdn.tiny.cloud/1/1dfx5sqlnqa2kcgpekbzff2tmwkf5dvtdo5lva61r7j74svx/tinymce/5/tinymce.min.js",
+        () => {
+          tinymce.init({
+            selector: "#mytextarea",
+            menubar: "edit",
+            init_instance_callback: editor => {
+              editor.on("input change undo redo setcontent", () => {
+                this.model.content = editor.getContent();
+              });
+            },
+            height: 350,
+            plugins: "fullscreen link lists code visualblocks",
+            toolbar:
+              "styleselect | fontsizeselect | link | numlist bullist | removeformat",
+            content_style:
+              ".text-left { text-align: left; } " +
+              ".text-center { text-align: center; } " +
+              ".text-right { text-align: right; } ",
+            fontsize_formats: "14px 15px 16px 17px 18px 20px 24px",
+            formats: {
+              heading_1: {
+                selector: "p, h1, h2, h3, h4, h5, h6",
+                classes: "heading--1"
+              },
+              heading_2: {
+                selector: "p, h1, h2, h3, h4, h5, h6",
+                classes: "heading--2"
+              },
+              heading_3: {
+                selector: "p, h1, h2, h3, h4, h5, h6",
+                classes: "heading--3"
+              },
+              heading_4: {
+                selector: "p, h1, h2, h3, h4, h5, h6",
+                classes: "heading--4"
+              },
+              alignleft: {
+                selector: "p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img",
+                classes: "text-left"
+              },
+              aligncenter: {
+                selector: "p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img",
+                classes: "text-center"
+              },
+              alignright: {
+                selector: "p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img",
+                classes: "text-right"
+              }
+            },
+            style_formats: [
+              {
+                title: "Block",
+                items: [
+                  {
+                    title: "Paragraph",
+                    format: "p"
+                  },
+                  {
+                    title: "H1",
+                    format: "h1",
+                    classes: "heading"
+                  },
+                  {
+                    title: "H2",
+                    format: "h2",
+                    classes: "heading"
+                  },
+                  {
+                    title: "H3",
+                    format: "h3",
+                    classes: "heading"
+                  },
+                  {
+                    title: "H4",
+                    format: "h4",
+                    classes: "heading"
+                  },
+                  {
+                    title: "H5",
+                    format: "h5",
+                    classes: "heading"
+                  },
+                  {
+                    title: "H6",
+                    format: "h6",
+                    classes: "heading"
+                  },
+                  {
+                    title: "Blockquote",
+                    format: "blockquote"
+                  }
+                ]
+              },
+              {
+                title: "Inline",
+                items: [
+                  {
+                    title: "Bold",
+                    icon: "bold",
+                    format: "bold"
+                  },
+                  {
+                    title: "Italic",
+                    icon: "italic",
+                    format: "italic"
+                  },
+                  {
+                    title: "Underline",
+                    icon: "underline",
+                    format: "underline"
+                  },
+                  {
+                    title: "Strikethrough",
+                    icon: "strike-through",
+                    format: "strikethrough"
+                  },
+                  {
+                    title: "Superscript",
+                    icon: "superscript",
+                    format: "superscript"
+                  },
+                  {
+                    title: "Subscript",
+                    icon: "subscript",
+                    format: "subscript"
+                  },
+                  { title: "Code", icon: "sourcecode", format: "code" }
+                ]
+              },
+              {
+                title: "Alignment",
+                items: [
+                  {
+                    title: "Text Right",
+                    icon: "align-right",
+                    format: "alignright"
+                  },
+                  {
+                    title: "Text Center",
+                    icon: "align-center",
+                    format: "aligncenter"
+                  },
+                  {
+                    title: "Text Left",
+                    icon: "align-left",
+                    format: "alignleft"
+                  }
+                ]
+              },
+              {
+                title: "Link Colors",
+                items: [
+                  {
+                    title: "Digi Green",
+                    selector: "a",
+                    classes: "link--secondary"
+                  },
+                  {
+                    title: "White",
+                    selector: "a",
+                    classes: "link--secondary-white"
+                  }
+                ]
+              },
+              {
+                title: "Heading Classes",
+                items: [
+                  {
+                    title: "Heading 1",
+                    format: "heading_1"
+                  },
+                  {
+                    title: "Heading 2",
+                    format: "heading_2"
+                  },
+                  {
+                    title: "Heading 3",
+                    format: "heading_3"
+                  },
+                  {
+                    title: "Heading 4",
+                    format: "heading_4"
+                  }
+                ]
+              }
+            ]
+          });
+        }
+      );
     }
   },
   watch: {
-    'model': {
-      handler: function (value) {
-        this.$emit('changed-model', value);
+    model: {
+      handler: function(value) {
+        this.$emit("changed-model", value);
       },
       deep: true
     }
   }
-}
+};
 </script>
 
-<style>
-  .p-metatags__google-title {
-    color: blue;
-    text-decoration: underline;
-  }
+<style lang="scss">
+.mce-tinymce {
+  box-sizing: border-box;
+}
 
-  .p-metatags__google-link {
-    color: green;
-  }
+.p-metatags__google-title {
+  color: blue;
+  text-decoration: underline;
+}
 
-  .p-metatags__preview {
-    margin: 5px 0 15px;
-    padding: 10px;
-    color: #000;
-    background: #FFF;
-  }
+.p-metatags__google-link {
+  color: green;
+}
+
+.p-metatags__preview {
+  margin: 5px 0 15px;
+  padding: 10px;
+  color: #000;
+  background: #fff;
+}
 </style>
